87a099075990961e74b7dc07ef90005162f1a683
woldt <woldtensae.aman@gmail.com>
2017-10-06 20:18:27 -0500

    commit blockservice service class

---
+++ mumSchedule/src/main/java/mucScheduler/service/BlockService.java
@@ -0,0 +1,20 @@
+package mucScheduler.service;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import mumSchedule.domain.Blocks;
+
+@Service
+public class BlockService {
+	public List<Blocks> getAllBlocks(){
+		return Arrays.asList(
+				new Blocks(LocalDate.of(2017, 12, 12), LocalDate.of(2017, 12, 12)),
+				new Blocks(LocalDate.of(2017, 11, 12), LocalDate.of(2017, 11, 12)),
+				new Blocks(LocalDate.of(2017, 10, 12), LocalDate.of(2017, 10, 12))
+		);
+	}
+}
--- mumSchedule/src/main/java/mumScheduler/controllers/BlocksController.java
+++ mumSchedule/src/main/java/mumScheduler/controllers/BlocksController.java
@@ -1,24 +1,21 @@
 package mumScheduler.controllers;
 
-import java.time.LocalDate;
-import java.util.Arrays;
+
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import mucScheduler.service.BlockService;
 import mumSchedule.domain.Blocks;
 
 @RestController
 public class BlocksController {
-	
+	@Autowired
+	private BlockService blockService;
 	@RequestMapping("/blocks")
 	public List<Blocks> readBlocks() {
-		return Arrays.asList(
-				new Blocks(LocalDate.of(2017, 12, 12), LocalDate.of(2017, 12, 12)),
-				new Blocks(LocalDate.of(2017, 11, 12), LocalDate.of(2017, 11, 12)),
-				new Blocks(LocalDate.of(2017, 10, 12), LocalDate.of(2017, 10, 12))
-		);
-				
+		return blockService.getAllBlocks();
 	}
 }
